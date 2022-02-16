9ec244a7cb9a9181122546e7127aab8029bb83e1
loveyousomuch <loveyousomuch554@gmail.com>
2022-02-16 11:23:02 +0300

    rename work

--- rename_file_vars.py
+++ rename_file_vars.py
@@ -1,1 +1,24 @@
 
+def rename(f):
+    words = {
+        "числитель": "numerator",
+        "знаменатель": "denominator",
+        "НОД": "GCD",
+        "числительСокращенный": "numeratorAbbreviated",
+        "знаменательСокращенный": "denominatorReduced"
+    }
+    newf = "renamed_"+f
+
+    with open(f, "r") as file:
+        content = file.read()
+    
+    for w_from, w_to in words.items():
+        content = content.replace(w_from, w_to)
+    
+    with open(newf, "w") as file:
+        file.write(content)
+
+if __name__ == "__main__":
+    filename = "Program.c"
+    rename(filename)
+
