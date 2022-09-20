8901e849a0e721cbeda0c9e126af46c8db441cb2
lusm <loveyousomuch554@gmail.com>
2022-09-20 23:19:06 +0300

    hmm, looks like python code

---
+++ practice-up03/17-sept-solution.py
@@ -0,0 +1,13 @@
+import random
+import string
+from typing import Any
+
+# logging
+
+def generat_input(elems_cnt: int = 5, max_size: int = 100, filling_entity: Any = string.ascii_letters) -> list[list]:
+    print(f"{elems_cnt=}, {max_size=}, {filling_entity=}")
+
+# 1. Генерировать рандомные массиыв
+# 2. Определить условие
+
+generat_input()
