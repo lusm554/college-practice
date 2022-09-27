2c05ec634b0f9810c7f6bfee6a5ba6a13a64eeab
lusm <loveyousomuch554@gmail.com>
2022-09-27 21:33:51 +0300

    mvp

--- practice-up03/black-box-method/calc.py
+++ practice-up03/black-box-method/calc.py
@@ -1,4 +1,37 @@
 from functools import reduce 
+from collections.abc import Callable
+
+INFO = 1
+CALC = 2
+EXIT = 3
+
+def get_mode() -> int:
+    menu = """
+    1. Get info about operations.
+    2. Calculate/compute operation.
+    3. Exit.
+    Input number(1, 2, 3).
+    """
+    print(menu)
+    mode = int(input("Action number: "))
+    print()
+    return mode
+
+def get_input() -> (str, list[int]):
+    cmd = input("Which command? ")
+    args = list(map(int, input("Input args by coma(like 1, 2, 3): ").split(", ")))
+    return (cmd, args)
+
+def get_oper(oper_name: str, opers: dict) -> Callable:
+    for op_names, op_entity in opers.items():
+        if oper_name in op_names:
+            return op_entity["action"]
+    raise ValueError(f"Operation {oper_name} not found.")
+    
+
+def show_info(opers: dict) -> None:
+    opers_names = "\n\n".join([f'{opbody["desc"]}:\n{", ".join(op)}' for op, opbody in opers.items()]) + "\n"
+    print(opers_names)
 
 def main():
     opers = {
@@ -22,14 +55,20 @@ def main():
 
     while 1:
         try:
-            print()
-            opers_names = "\n\n".join([f'{opbody["desc"]}:\n{", ".join(op)}' for op, opbody in opers.items()]) + "\n"
-            print(opers_names)
-            cmd = input("Which command? ")
-            args = list(map(int, input("Input args by coma(like 1, 2, 3): ").split(", ")))
-            # iter
+            mode = get_mode()
+            if mode == INFO:
+                show_info(opers) 
+            elif mode == CALC:
+                cmd, args = get_input()
+                oper = get_oper(cmd, opers)
+                res = oper(*args)
+                print(f"Result: {res}")
+                print()
+            elif mode == EXIT:
+                exit()
         except Exception as e:
             print(f"Exception: {e}")
+            print()
         
         
 
