1138caa4c51a9a8d513e0ea4062957a86b0a648a
lusm <loveyousomuch554@gmail.com>
2022-11-06 21:49:48 +0300

    faster way to create tables

---
+++ university-information-system/parse.py
@@ -0,0 +1,21 @@
+with open("t.ttxt", "r") as f:
+    r = {}
+    tname = ""
+    for line in f:
+        if ":" in line:
+            tname = line[line.find('"')+1:line.rfind('"')]
+        if line.count("-") >= 2:
+            l = line.split()[1]
+            if tname in r:
+                r[tname].append(l)
+            else:
+                r[tname] = []
+                r[tname].append(l)
+
+for t, v in r.items():
+    fields = [f"    {i} INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL" if ii==0 \
+              else f"    {i}" for ii, i in enumerate(v)]
+    fields = ",\n".join(fields)
+    shape = \
+    f"""CREATE TABLE {t} (\n{fields}\n);"""
+    print(shape)
