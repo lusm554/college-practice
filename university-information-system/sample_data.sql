f3b501859084c3f2c63bea9903f22040951aec8c
lusm <loveyousomuch554@gmail.com>
2022-11-06 22:13:58 +0300

    test version of sql

--- university-information-system/sample_data.sql
+++ university-information-system/sample_data.sql
@@ -9,7 +9,71 @@ CREATE TABLE students (
     scholarship_amt INTEGER
 );
 
+INSERT INTO students
+VALUES
+    (NULL, 1, 1, 'm', '2000', '21', 0, 0);
 
-INSERT INTO students VALUES
-    (null, 1, 2, 'm', '2000', 21, 0, 0),
-    (null, 1, 2, 'm', '2000', 21, 0, 0)
+CREATE TABLE teachers (
+    teacher_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    faculty_id INTEGER,
+    category TEXT,
+    gender TEXT,
+    birth_year TEXT,
+    age INTEGER,
+    cnt_of_children INTEGER,
+    salary_amt INTEGER,
+    is_graduate_student INTEGER
+);
+
+INSERT INTO teachers
+VALUES
+    (NULL, 1, 'ассистент', 'm', '1986', 36, 1, 10000, 0);
+
+CREATE TABLE groups (
+    group_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT,
+    course INTEGER,
+    faculty_id INTEGER,
+    department_id INTEGER
+);
+
+INSERT INTO groups
+VALUES
+    (NULL, '11IS-18', 1, 1, 1);
+
+CREATE TABLE dissertations (
+    dissertation_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    author_id INTEGER, -- problem, because teacher and student id maybe the same
+    defense_date TEXT,
+    title TEXT
+);
+
+INSERT INTO dissertations
+VALUES
+    (NULL, 1, '2000', 'some title');
+
+CREATE TABLE Faculty (
+    faculty_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+);
+ 
+INSERT INTO Faculty
+VALUES
+    (NULL);
+
+CREATE TABLE Departments (
+    department_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    faculty_id INTEGER
+);
+
+INSERT INTO Departments
+VALUES
+    (NULL, 1);
+
+CREATE TABLE Disciplines (
+    discipline_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT
+);
+
+INSERT INTO Disciplines
+VALUES
+    (NULL, "CS");
